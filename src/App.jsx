29044f4df950d434a2643fc1698a5ae454c7f391
Hajarivelo1 <93727504+Hajarivelo1@users.noreply.github.com>
2023-03-15 21:21:02 +0300

    Premier commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Review from "./Review";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Our Reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
+}
+
+export default App;
